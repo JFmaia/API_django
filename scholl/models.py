dadd3eefeda8f94ebf449b22363f2d92e059e752
JFmaia <jfmaia741@gmail.com>
2022-05-18 20:07:46 -0300

    Criando modelo aluno.

--- scholl/models.py
+++ scholl/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100),
+    rg = models.CharField(max_length=9),
+    email = models.EmailField(max_length=100),
+    idade = models.IntegerField(),
+
+    def __str__(self):
+        return self.nome
+
+    
+    
