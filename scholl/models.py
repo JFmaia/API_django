e2b383d1a3d66a8e534bbfe8920f8ee1c76d6775
JFmaia <jfmaia741@gmail.com>
2022-05-18 20:30:23 -0300

    Colocando null como verdadeiro, aceitando null.

--- scholl/models.py
+++ scholl/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
 class Aluno(models.Model):
-    nome = models.CharField(max_length=100),
-    rg = models.CharField(max_length=9),
-    email = models.EmailField(max_length=100),
-    idade = models.IntegerField(),
+    name = models.CharField(max_length=30, null=True)
+    rg = models.CharField(max_length=9, null=True)
 
     def __str__(self):
-        return self.nome
+        return self.name
 
     
     
