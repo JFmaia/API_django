128c15022838699430e8f14d517316aff70ed949
JFmaia <jfmaia741@gmail.com>
2022-05-19 13:39:26 -0300

    Restart...

--- scholl/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from scholl.models import Aluno
-
-class Alunos(admin.ModelAdmin):
-    list_display = ('name', 'rg')
-    list_display_links = ('name', 'rg')
-    search_fields = ('name',)
-
-admin.site.register(Aluno, Alunos)
